6265e55f5d98950002ec62df8e07dcbda3f3bd0e
ayoubkamli <ayyoub.kamli@gmail.com>
2021-10-13 18:46:05 +0200

    add multirepo

--- lib/main.dart
+++ lib/main.dart
@@ -32,37 +32,47 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return RepositoryProvider(
-      create: (context) => AuthRepository(),
-      child: RepositoryProvider(
-        create: (context) => PlaylistRepository(),
-        child: RepositoryProvider(
-          create: (context) => EventRepository(),
-          child: MultiBlocProvider(
-            providers: [
-              BlocProvider(
-                create: (context) =>
-                    SessionCubit(authRepo: context.read<AuthRepository>()),
-              ),
-              BlocProvider(create: (BuildContext context) {
-                return EventCubit(
-                  eventRepository: context.read<EventRepository>(),
-                );
-              }),
-              BlocProvider(create: (BuildContext newcontext) {
-                return PlaylistCubit(
-                  playlistRepository: newcontext.read<PlaylistRepository>(),
-                );
-              })
-            ],
-            child: MaterialApp(
-              debugShowCheckedModeBanner: false,
-              home: AppNavigator(),
-            ),
+    return MultiRepositoryProvider(
+
+        /// create: (context) => AuthRepository(),
+        providers: [
+          RepositoryProvider(create: (context) => EventRepository()),
+          RepositoryProvider(create: (context) => AuthRepository()),
+          RepositoryProvider(
+            create: (context) => PlaylistRepository(),
           ),
-        ),
-      ),
-    );
+        ],
+
+        /// child: RepositoryProvider(
+        ///   create: (context) => PlaylistRepository(),
+        ///   child: RepositoryProvider(
+        ///     create: (context) => EventRepository(),
+        child: MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) =>
+                  SessionCubit(authRepo: context.read<AuthRepository>()),
+            ),
+            BlocProvider(create: (BuildContext context) {
+              return EventCubit(
+                eventRepository: context.read<EventRepository>(),
+              );
+            }),
+            BlocProvider(create: (BuildContext newcontext) {
+              return PlaylistCubit(
+                playlistRepository: newcontext.read<PlaylistRepository>(),
+              );
+            })
+          ],
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: AppNavigator(),
+          ),
+        ));
+
+    ///     ),
+    ///   ),
+    /// );
   }
 
   Future<void> _isLoggedIn() async {
